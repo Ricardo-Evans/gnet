62ac4d82c6365bf560ad6530f5c24b7adfaea3e4
Ricardo Evans <ricardo.a.evans@gmail.com>
2021-12-27 16:27:51 +0800

    Fix: thread safety issue for load balancer of client

--- client_unix.go
+++ client_unix.go
@@ -1,22 +1,28 @@
-//+build linux freebsd dragonfly darwin
+//go:build linux || freebsd || dragonfly || darwin
+// +build linux freebsd dragonfly darwin
 
 package gnet
 
 import (
 	"github.com/panjf2000/gnet/internal/socket"
+	"sync"
 )
 
 type client struct {
 	server *Server
+	mutex  *sync.Mutex // used to provide thread safety, for load balancer only currently
 }
 
 func newClient(server *Server) *client {
 	return &client{
-		server,
+		server: server,
+		mutex:  &sync.Mutex{},
 	}
 }
 
 func (c *client) Dial(addr string) (Conn, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	network, address := parseProtoAddr(addr)
 	sa, na, err := socket.GetUDPSocketAddr(network, address)
 	if err != nil {
